8836c62f979f5f41b0beb626f0cac3c3979d318d
njsaugat <njsaugat@gmail.com>
2023-12-04 18:23:56 +0545

    done

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,23 @@
-export default {
+import type { Config } from "@jest/types";
+
+const esModules = ["lodash-es", "nanoid"].join("|");
+const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.[jt]sx?$": "ts-jest",
+
     // process `*.tsx` files with `ts-jest`
   },
+  transformIgnorePatterns: [
+    `/node_modules/(?!${esModules})`,
+    "\\.pnp\\.[^\\/]+$",
+  ],
   moduleNameMapper: {
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+    // "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+    "^lodash-es(/(.*)|$)": "lodash$1",
+    "^nanoid(/(.*)|$)": "nanoid$1",
   },
 };
+
+export default config;
--- src/features/auth/Components/__tests__/login.test.tsx
+++ src/features/auth/Components/__tests__/login.test.tsx
@@ -1,35 +1,10 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../../../../App";
 import Login from "../Login";
 import * as ReactDOM from "react-dom";
-test("demo", () => {
-  expect(true).toBe(true);
-});
-
-test("Renders the main page", () => {
-  //   render(<App />);
-  render(<App />);
-  expect(true).toBeTruthy();
-});
-
-describe("Login component tests", () => {
-  let container: HTMLDivElement;
-  beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-    ReactDOM.render(<Login />, container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-    container.remove();
-  });
-  it("Renders all inputs fields correctly", () => {
-    const inputs = container.querySelectorAll("input");
-    expect(inputs).toHaveLength(2);
-
-    expect(inputs[0].name.toLowerCase()).toBe("email");
-    expect(inputs[1].name.toLowerCase()).toBe("email");
-  });
+test("renders learn react link", () => {
+  render(<Login />);
+  const linkElement = screen.getByText(/email/i);
+  expect(linkElement).toBeInTheDocument();
 });
