3a26471ca4392637e55cc72b996a6f0be479e848
njsaugat <njsaugat@gmail.com>
2023-12-23 20:33:46 +0545

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,16 @@ import type { Config } from "@jest/types";
 const esModules = ["lodash-es", "nanoid"].join("|");
 
 const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
   transform: {
     "^.+\\.tsx?$": "ts-jest",
-    // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^lodash-es(/(.*)|$)": "lodash$1",
+    "^nanoid(/(.*)|$)": "nanoid$1",
   },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 };
 
 export default config;
